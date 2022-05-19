88b80fadf7c6aa010805d904972fff7b1a8c01fa
nandithagitub <78301156+nandithagitub@users.noreply.github.com>
2022-05-19 20:54:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+song="";
+function play(){
+ song.play("Dynamite.mp3");
+}
+song1="";
+function play1(){
+ song1.play("piper.mp3");
+}
+song2="";
+function play2(){
+ song2.play("Dynamite.mp3");
+}
+song3="";
+function play3(){
+ song3.play("Dynamite.mp3");
+}
+song4="";
+function play4(){
+ song4.play("friends.mp3");
+}
+song5="";
+function play5(){
+ song5.play("BWLuv.mp3");
+}
+song6="";
+function play6(){
+ song6.play("filter.mp3");
+}
+song7="";
+function play7(){
+ song7.play("euph.mp3");
+}
+song8="";
+function play8(){
+ song8.play("IDOL.mp3");
+}
+song9="";
+function play9(){
+ song9.play("Film Out.mp3");
+}
+
